13ce299c4f236f99fa47ec894607a70801d57298
szzelln <127702953+szzelln@users.noreply.github.com>
2026-05-09 02:26:49 +0900

    Implement title & scene manager
    - 타이틀 화면을 구성하고 버튼별 기능 연결(챌린지는 씬이 없어서 비워둠)
    - SceneManager : 씬 로드 및 현재 씬 재시작 기능 구현
    - SceneManager : 게임 종료 기능 구현

---
+++ Assets/02_Scripts/Managers/GameSceneManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public static GameSceneManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    #region 씬 전환
+    public void LoadScene(string sceneName)
+    {
+        // todo : 다음 씬에 필요한 에셋 로드
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+    #endregion
+
+
+    #region 현재 씬 다시 로드
+    public void RestartCurrentScene()
+    {
+        // 현재 활성화된 씬 이름 가져오기
+        string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+
+        // todo : 저장 된 게임 데이터 불러오기
+
+        // 현재 씬 다시 로드
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneName);
+
+    }
+    #endregion
+
+
+    #region 게임종료
+    public void QuitGame()
+    {
+        Debug.Log("게임 종료");
+
+        // 빌드된 게임에서 종료
+        Application.Quit();
+
+#if UNITY_EDITOR
+        // 유니티 에디터에서 플레이 모드 종료
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    #endregion
+}
